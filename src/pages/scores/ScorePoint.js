047c59ec85744def357d42cbb821fcfade04e571
wing <nguyenbuivn11@gmail.com>
2021-02-07 14:32:53 +0700

    clean code organization

--- src/pages/scores/ScorePoint.js
+++ src/pages/scores/ScorePoint.js
@@ -1,13 +1,10 @@
-import React from 'react'
-import { useCounterUp } from '../../hooks/useCounterUp'
-import { ItemPoint } from './StyledScores'
+import React from 'react';
+import { useCounterUp } from '../../hooks/useCounterUp';
+import { ItemPoint } from './StyledScores';
 
-const ScorePoint = ({score}) => {
-    const counter = useCounterUp({ num: score, times: 10});
-
-    return (
-        <ItemPoint>{counter}pt</ItemPoint>
-    )
-}
+const ScorePoint = ({ score }) => {
+  const counter = useCounterUp({ num: score, times: 10 });
+  return <ItemPoint>{counter}pt</ItemPoint>;
+};
 
 export default ScorePoint;
